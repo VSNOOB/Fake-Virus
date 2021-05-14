925c7a12f2ce009b04a1598c39487ed6be291f1c
VSNOOB <83365959+VSNOOB@users.noreply.github.com>
2021-05-14 09:43:14 -0700

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,1 +1,21 @@
-@echo off -c
+@echo off 
+echo would you like to scan your PC?
+pause
+echo scanning
+pause
+dir/s
+echo Scan Complete
+pause
+cls
+echo VIRUS DETECTED
+pause
+cls
+echo VIRUS IS DELETING YOUR BANK ACCOUNT, DO YOU WANT TO STOP IT?
+pause 
+cls
+echo ACCESS DENIED
+echo ACCESS DENIED
+echo ACCESS DENIED
+pause
+cls
+echo DELETING YOUR FILES
